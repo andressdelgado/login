697c0243f0287eb686275564a8b9803ad48f48e8
andressdelgado <andresdelgadoherruzo@gmail.com>
2023-12-14 13:01:01 +0100

    cambios, subido para hosting

--- config/configdb.php
+++ config/configdb.php
@@ -1,8 +1,8 @@
 <?php
-    define('USUARIO', 'root');
-    define('SERVIDOR', 'localhost');
-    define('CONTRASENIA', '');
-    define('BBDD', 'minijuegos2');
+    define('USUARIO', 'user2daw_04');
+    define('SERVIDOR', '2daw.esvirgua.com');
+    define('CONTRASENIA', '1Z~%4D+*9BTc');
+    define('BBDD', 'user2daw_BD1-04');
 
     define("CONTROLADOR_DEFECTO", "cInicio");
     define("METODO_DEFECTO", "comprobarPaginaInicio");    
